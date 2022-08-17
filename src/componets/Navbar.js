6ec05a78aaa9073023156aa51b69d71ce885908b
Exe <exeferreira7@gmail.com>
2022-08-17 17:35:39 -0300

    Agregados modulos de iconos y responsibilidad

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -4,35 +4,136 @@ import {
   Button,
   Toolbar,
   Typography,
-  Link,
   IconButton,
+  Container,
+  Menu,
+  MenuItem,
+  Box,
 } from "@mui/material";
-import { Box } from "@mui/system";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+
+const pages = [
+  {
+    text: "Inicio",
+    direction: "/",
+  },
+  {
+    text: "Alimentos Caninos",
+    direction: "/perros",
+  },
+  {
+    text: "Alimentos Felinos",
+    direction: "/gatos",
+  },
+  {
+    text: "Accesorios",
+    direction: "/accesorios",
+  },
+];
 
 const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <Box sx={{ flexGrow: 3, marginBottom: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{ display: "flex", justifyContent: "space-around" }}>
-          <Button href="/" variant="text" color="inherit">
-            Inicio
-          </Button>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.text} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">
+                    <Link
+                      to={page.direction}
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      {page.text}
+                    </Link>
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
 
-          <Button href="/perros" variant="text" color="inherit">
-            Alimentos Caninos
-          </Button>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.text}
+                onClick={handleCloseNavMenu}
+                color="inherit"
+              >
+                <Link
+                  to={page.direction}
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  {page.text}
+                </Link>
+              </Button>
+            ))}
+          </Box>
 
-          <Button href="/gatos" variant="text" color="inherit">
-            Alimentos Felinos
-          </Button>
-
-          <Button href="/accesorios" variant="text" color="inherit">
-            Accesorios
-          </Button>
+          <Box sx={{ flexGrow: 0 }}>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            ></Menu>
+          </Box>
         </Toolbar>
-      </AppBar>
-    </Box>
+      </Container>
+    </AppBar>
   );
 };
-
 export default Navbar;
