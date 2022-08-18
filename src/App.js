bec15b5f6f0e55ae2616c28137ef15bd0f0499b4
Diego Mercado <diego.mercado1997@hotmail.com>
2022-08-18 10:50:43 -0300

    footer con medios de pago

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 
+import Footer from './components/Footer';
 import MarcasDestacada from './components/MarcasDestacadas'
 
 function App() {
   return (
     <div className="App">
       <MarcasDestacada />
+      <Footer/>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -66,7 +66,24 @@ export default function Footer() {
         </Grid>
     </Grid>
     <Divider variant='middle'/>
+    <Grid container justifyContent="center"  padding="3rem">
+        <Grid item xs={12} md={12} lg={4}>
+            <Typography variant="h6" color="primary" ml={2}>Medios de Pago</Typography>
+        </Grid>
+        <Grid item xs={12} md={12} lg={8}>
+            {tarjetas.map(el=> <img alt={el.alt} src={el.src} width="40px"/>)}
+        </Grid>
+    </Grid>
     </>
    
   )
 }
+
+const tarjetas = [
+{id:0 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/visa@2x.png",alt:"Visa"},
+{id:1 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/mastercard@2x.png",alt:"Mastercard"},
+{id:2 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/amex@2x.png",alt:"AmericaExpress"},
+{id:3 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/tarjeta-naranja@2x.png",alt:"Naranja"},
+{id:4 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/nativa@2x.png",alt:"Nativa"},
+{id:5 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/mastercarddebit@2x.png",alt:"Maestro"}
+]
