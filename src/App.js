d30260980d82c65be54faa87455a239f90caff9a
jorge sanchez gonzalez <j.sanchezgonzalez@hotmail.com>
2022-08-19 00:59:44 -0400

    subiendo cambios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import MarcasDestacada from './components/MarcasDestacadas'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MarcasDestacada />
     </div>
   );
