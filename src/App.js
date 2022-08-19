486c1386a17398645c2d83a1165add627122fb9b
jorge sanchez gonzalez <j.sanchezgonzalez@hotmail.com>
2022-08-19 16:54:00 -0400

    subir componentes nuevos agregar header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@
 import Detalle from './components/Detalle';
 import Footer from './components/Footer';
 import MarcasDestacada from './components/MarcasDestacadas'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
     <Detalle />
       <MarcasDestacada />
       <Footer/>
