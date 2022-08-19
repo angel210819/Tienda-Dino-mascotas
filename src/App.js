e526e08a1df02f791cf790634874e685140b939d
angel210819 <89662810+angel210819@users.noreply.github.com>
2022-08-18 22:42:25 -0300

    se agrega la seccion de detalles de politica

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 
+import Detalle from './components/Detalle';
 import Footer from './components/Footer';
 import MarcasDestacada from './components/MarcasDestacadas'
 
 function App() {
   return (
     <div className="App">
+    <Detalle />
       <MarcasDestacada />
       <Footer/>
     </div>
