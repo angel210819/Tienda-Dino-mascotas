12dee412e0973ca9269660a5e20f5668a835b4bc
jorge sanchez gonzalez <j.sanchezgonzalez@hotmail.com>
2022-08-21 15:34:47 -0400

    cambios a rama main

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,32 +2,37 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-//import Typography from '@mui/material/Typography';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import SearchBar from './SearchBar';
+import Logo from './../images/dinoNegro.png'
+import Fono from './../images/fono.png'
+import Carro from './../images/carroCompras.png'
+import SearchBar from './SearchBar'
 
 
-export default function DenseAppBar() {
+
+
+export default function ButtonAppBar() {
   return (
-    <Box sx={{ display: { xs: 'none', md: 'flex' },  alignItems: "flex-end" }}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IU2S9XtjmHtU4crrQmrDi_wAgBsUutfISg&usqp=CAU" alt="Dino" height="25" width="25" />
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+           <img src={Logo} alt="Dino" height="45" width="45" />
           </IconButton>
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ ml: 65 }}>
-            <SearchBar  /> 
-          </IconButton>
-         
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 10 }}>
-          <img src="https://st2.depositphotos.com/4060975/8056/v/600/depositphotos_80565476-stock-illustration-magnifier-vector-icon.jpg" alt="Dino" height="25" width="25" />
-          </IconButton>
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ ml: 55 }}>
-          <img src="https://w7.pngwing.com/pngs/225/984/png-transparent-computer-icons-shopping-cart-encapsulated-postscript-shopping-cart-angle-black-shopping.png" alt="Dino" height="25" width="25" />
-          </IconButton>
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAzjMxvIPK2fFWZuvqRlIKxpgsfgTUoEOyZCC3dRuhUwQVfNEnDehQMElK8ld1k7k2TZY&usqp=CAU" alt="Dino" height="25" width="25" />
-          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Dino Mascotas
+          </Typography>
+          <SearchBar />
+          <Button color="inherit"><img src={Carro} alt="Carro" height="30" width="30" /></Button>
+          <Button color="inherit"><img src={Fono} alt="Fono" height="30" width="30" /></Button>
         </Toolbar>
       </AppBar>
     </Box>
