a02c3e4c62c1d70fcbb2953c2962e5d0de8b748c
Exe <exeferreira7@gmail.com>
2022-08-25 19:43:28 -0300

    Axios funcionando y algunos warning quitados

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,89 +1,125 @@
-import React from 'react'
-import Grid from '@mui/material/Grid';
-import { Link, ListItem, ListItemText, Typography } from '@mui/material';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import List from '@mui/material/List';
-import PhoneIcon from '@mui/icons-material/Phone';
-import MailIcon from '@mui/icons-material/Mail';
-import RoomIcon from '@mui/icons-material/Room';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import Divider from '@mui/material/Divider';
-
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { Link, ListItem, ListItemText, Typography } from "@mui/material";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import List from "@mui/material/List";
+import PhoneIcon from "@mui/icons-material/Phone";
+import MailIcon from "@mui/icons-material/Mail";
+import RoomIcon from "@mui/icons-material/Room";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import Divider from "@mui/material/Divider";
 
 export default function Footer() {
   return (
     <>
-    <Grid 
+      <Grid
         container
         spacing={2}
         color="primary"
         justifyContent="center"
         padding="3rem"
-        >
+      >
         <Grid item xs={12} md={4} lg={4}>
-            <List>
+          <List>
             <ListItem>
-                <Link href="/#">Términos y condiciones</Link>
+              <Link href="/#">Términos y condiciones</Link>
             </ListItem>
             <ListItem>
-                <Link  href="/#">Politica y privacidad</Link>
+              <Link href="/#">Politica y privacidad</Link>
             </ListItem>
             <ListItem>
-                <Link href="/#">Politica de Devolucion y Cambios</Link>
+              <Link href="/#">Politica de Devolucion y Cambios</Link>
             </ListItem>
             <ListItem>
-                <Link href="/#">Preguntas Frecuentes</Link>
+              <Link href="/#">Preguntas Frecuentes</Link>
             </ListItem>
-            </List>
+          </List>
         </Grid>
 
         <Grid item xs={12} md={4} lg={4}>
-            <Typography variant='h6' color="primary" ml={2}>Contactanos</Typography>
-           <List >
-              <ListItem>
-                    <WhatsAppIcon color='primary'/>
-                    <Link href='/#' >5493513845741</Link>
-              </ListItem>
-              <ListItem>
-                    <PhoneIcon color='primary'/>
-                    <Link href='/#'>3513845741 (Solo Via Whatsapp)</Link>
-              </ListItem>
-              <ListItem>
-                    <MailIcon color='primary'/>
-                    <ListItemText > <Link href='/#' >nose@numen.com</Link></ListItemText>
-              </ListItem>
-              <ListItem>
-                    <RoomIcon color='primary'/> 
-                    <Link href='/#' >Rafael Nuñez 4430</Link>
-              </ListItem>
-           </List>
-         </Grid>
+          <Typography variant="h6" color="primary" ml={2}>
+            Contactanos
+          </Typography>
+          <List>
+            <ListItem>
+              <WhatsAppIcon color="primary" />
+              <Link href="/#">5493513845741</Link>
+            </ListItem>
+            <ListItem>
+              <PhoneIcon color="primary" />
+              <Link href="/#">3513845741 (Solo Via Whatsapp)</Link>
+            </ListItem>
+            <ListItem>
+              <MailIcon color="primary" />
+              <ListItemText>
+                {" "}
+                <Link href="/#">nose@numen.com</Link>
+              </ListItemText>
+            </ListItem>
+            <ListItem>
+              <RoomIcon color="primary" />
+              <Link href="/#">Rafael Nuñez 4430</Link>
+            </ListItem>
+          </List>
+        </Grid>
 
         <Grid item xs={12} md={4} lg={4}>
-            <Typography variant="h6" color="primary" mb={2}>Sigamos Conectados</Typography>
-            <Link href="/#" fontSize={4}><InstagramIcon color="primary" /><FacebookIcon color="primary"/></Link>
+          <Typography variant="h6" color="primary" mb={2}>
+            Sigamos Conectados
+          </Typography>
+          <Link href="/#" fontSize={4}>
+            <InstagramIcon color="primary" />
+            <FacebookIcon color="primary" />
+          </Link>
         </Grid>
-    </Grid>
-    <Divider variant='middle'/>
-    <Grid container justifyContent="center"  padding="3rem">
+      </Grid>
+      <Divider variant="middle" />
+      <Grid container justifyContent="center" padding="3rem">
         <Grid item xs={12} md={12} lg={4}>
-            <Typography variant="h6" color="primary" ml={2}>Medios de Pago</Typography>
+          <Typography variant="h6" color="primary" ml={2}>
+            Medios de Pago
+          </Typography>
         </Grid>
         <Grid item xs={12} md={12} lg={8}>
-            {tarjetas.map(el=> <img alt={el.alt} src={el.src} width="40px"/>)}
+          {tarjetas.map((el) => (
+            <img key={el.id} alt={el.alt} src={el.src} width="40px" />
+          ))}
         </Grid>
-    </Grid>
+      </Grid>
     </>
-   
-  )
+  );
 }
 
 const tarjetas = [
-{id:0 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/visa@2x.png",alt:"Visa"},
-{id:1 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/mastercard@2x.png",alt:"Mastercard"},
-{id:2 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/amex@2x.png",alt:"AmericaExpress"},
-{id:3 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/tarjeta-naranja@2x.png",alt:"Naranja"},
-{id:4 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/nativa@2x.png",alt:"Nativa"},
-{id:5 , src:"https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/mastercarddebit@2x.png",alt:"Maestro"}
-]
+  {
+    id: 0,
+    src: "https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/visa@2x.png",
+    alt: "Visa",
+  },
+  {
+    id: 1,
+    src: "https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/mastercard@2x.png",
+    alt: "Mastercard",
+  },
+  {
+    id: 2,
+    src: "https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/amex@2x.png",
+    alt: "AmericaExpress",
+  },
+  {
+    id: 3,
+    src: "https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/ar/tarjeta-naranja@2x.png",
+    alt: "Naranja",
+  },
+  {
+    id: 4,
+    src: "https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/nativa@2x.png",
+    alt: "Nativa",
+  },
+  {
+    id: 5,
+    src: "https://d26lpennugtm8s.cloudfront.net/assets/common/img/logos/payment/new_logos_payment/mastercarddebit@2x.png",
+    alt: "Maestro",
+  },
+];
--- src/components/Prueba.js
+++ src/components/Prueba.js
@@ -1,21 +1,35 @@
-import axios from 'axios'
-import React from 'react'
-
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Prueba = () => {
-    const [post, setPost] = React.useState(null);
+  const [post, setPost] = useState([]);
 
-    React.useEffect(() => {
-      axios.get("http://localhost:5000/Felinos").then((response) => {
-        setPost(response.data);
-      });
-    }, []);
-    console.log(post);
-    
+  const obtenerDatos = async () => {
+    const datos = await axios.get("http://localhost:5000/Felinos");
+    const comidas = await datos.data;
+    setPost(comidas);
+  };
+
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
+
   return (
-    <div>Prueba</div>
-  )
-}
+    <div>
+      {post.length > 0 ? (
+        post.map((comida, i) => (
+          <div key={comida.id}>
+            <h1>{comida.marca}</h1>
+            <img src={comida.img} alt={comida.nombre} />
+            <h6> Nombre: {comida.nombre}</h6>
+            <p>{comida.descripcion}</p>
+          </div>
+        ))
+      ) : (
+        <h1>No hay alimento, el bicho se va a cagar de hambre</h1>
+      )}
+    </div>
+  );
+};
 
-export default Prueba
+export default Prueba;
