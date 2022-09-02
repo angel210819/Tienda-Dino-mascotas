e20b324c3089cf11bf147c738ad51edd673f02ac
Exe <exeferreira7@gmail.com>
2022-09-02 17:49:02 -0300

    contador de productos en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,24 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Logo from './../images/dinoNegro.png'
-import Fono from './../images/fono.png'
-import Carro from './../images/carroCompras.png'
-import SearchBar from './SearchBar'
-import Badge from '@mui/material/Badge';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Logo from "./../images/dinoNegro.png";
+import Fono from "./../images/fono.png";
+import Carro from "./../images/carroCompras.png";
+import SearchBar from "./SearchBar";
+import Badge from "@mui/material/Badge";
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
-
-
+import { CartContex } from "../components/Contex/CartContex";
+import { useContext } from "react";
 
 export default function ButtonAppBar() {
-
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
+  const { cart } = useContext(CartContex);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -44,19 +31,27 @@ export default function ButtonAppBar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-          <img src={Logo} alt="Dino" height="45" width="45" />
+            <img src={Logo} alt="Dino" height="45" width="45" />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Dino Mascotas.
           </Typography>
           <SearchBar />
-          <Button color="inherit" onClick={handleOpen} open={open}
-             onClose={handleClose} href="./pruebabotoncarro" target="_blank">
-            <Badge badgeContent={4} color="secondary" >
-          <img src={Carro} alt="Carro" height="30" width="30" />
-          </Badge>
+          <Button
+            color="inherit"
+            onClick={handleOpen}
+            open={open}
+            onClose={handleClose}
+            href="./pruebabotoncarro"
+            target="_blank"
+          >
+            <Badge badgeContent={cart.length} color="secondary">
+              <img src={Carro} alt="Carro" height="30" width="30" />
+            </Badge>
           </Button>
-          <Button color="inherit"><img src={Fono} alt="Fono" height="30" width="30" /></Button>
+          <Button color="inherit">
+            <img src={Fono} alt="Fono" height="30" width="30" />
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
