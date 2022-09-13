be49578eeb15b2520aa160ffa001590ae32e0a95
Viviana fiocchi <fiocchiv2@gmail.com>
2022-09-12 22:10:29 -0300

    cambios 2  carrusel Instagram

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -3,7 +3,7 @@ import MarcasDestacadas from "./MarcasDestacadas";
 import Detalle from "./Detalle";
 import CallActions from "./CallActions";
 import Services from "./Services";
-import CarouselInstagram from "./CarouselInstagram";
+import CarouselInstagram from "./carruselInstagram/CarouselInstagram";
 import Slider from "./carousel/Slider";
 import Box from '@mui/material/Box';
 
